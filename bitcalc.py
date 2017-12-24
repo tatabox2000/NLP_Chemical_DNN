b61025b18b9968b6c3d6753a5c723a53db760b2f
DESKTOP-R50723I\tatab <tatabox2000@gmail.com>
2017-12-25 00:57:59 +0900

    cluster
    cluster

--- bitcalc.py
+++ bitcalc.py
@@ -1,8 +1,25 @@
 import numpy as np
 import pandas as pd
-import
+from rdkit import Chem
+from rdkit.Chem import rdDepictor
+from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit.Chem.Draw import IPythonConsole
+from IPython.display import SVG
+from rdkit.Chem import Draw
+from rdkit import rdBase
+from rdkit.Chem import AllChem
+from rdkit.Chem import rdMolDescriptors
+from rdkit import DataStructs
+
 def calc_bit(df):
-    print(df['cactvs_fingerprint'])
+    print()
+    fin1= np.array(list(df['cactvs_fingerprint'][0]))
+    fin2 = np.array(list(df['cactvs_fingerprint'][1]))
+    print(fin1,fin2)
+
+    result = DataStructs.FingerprintSimilarity(fin1,fin2)
+    print(result)
+
     num =list('0101001001010101011111')
     print(num)
     array = np.array(num)
@@ -10,5 +27,5 @@ def calc_bit(df):
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('connect_result.csv',dtype={'cactvs_fingerprint':'object'})
+    df = pd.read_csv('C:\\Users\\admin\\OneDrive\\NLP\\connect_result.csv',dtype={'cactvs_fingerprint':'object'})
     calc_bit(df)
---
+++ spectralClustering.py
@@ -0,0 +1,88 @@
+import time
+import warnings
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import cluster, datasets, mixture
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+from itertools import cycle, islice
+
+np.random.seed(0)
+import  pandas as pd
+import seaborn as sns
+
+def coor_csv2cluster(n = 500):
+    #test = np.random.rand(n,n)
+    # lam = 0.1
+    # test = np.random.exponential(1. / lam, size=n)
+    # test = np.random.f(30,30,n)
+    # test = test/test.max()
+    # test = 1 - test
+    # plt.subplot(3,1,1)
+    # plt.hist(test,100)
+    # test = np.triu(test,k=1)
+    # df = pd.DataFrame(test)
+    df2 = pd.read_csv('MACCSKeys_tanimoto.csv',skiprows=1,header=None)
+    #n = df2.shape[0]
+    #print(n)
+    df2 = df2.replace('None',0)
+    df2 = df2.fillna(0)
+    df2 = df2.where(df2.notnull(), 0)
+    index = df2.iloc[:,0]
+    df = df2.drop(0,axis=1)
+    df = df.iloc[:n,:n]
+    df = df.astype('float')
+
+    df[df == 1] = 0
+
+    # for i,chack in enumerate(df.isnull().any()):
+    #     if chack == True:
+    #         print(i,chack)
+    #     else:
+    #         pass
+
+    #df[df >0.8] = 1
+    # df[(df > 0.6) & (df <= 0.75)] = 0.0
+    # df[(df > 0.4) & (df <= 0.6)] = 0.0
+    # df[(df > 0.2) & (df <= 0.4)] = 0.0
+    # df[df <0.2] = 0
+
+    for i in np.arange(0,n,1):
+        for j in np.arange(0,n,1):
+            if i == j:
+                break
+            else:
+                df.iat[i,j] = df.iat[j,i]
+    plt.subplot(3,1,2)
+
+    sns.heatmap(df, fmt='g',cmap = "coolwarm")
+    #sns.heatmap(df)
+
+
+    spectral = cluster.SpectralClustering(
+        n_clusters=30, eigen_solver='arpack',
+        affinity='precomputed')
+    result = spectral.fit(df)
+    y_pred = result.labels_.astype(np.int)
+    df2= pd.DataFrame(y_pred)
+    df3 = df2.sort_values(by=[0],ascending=True)
+    index= index.iloc[df3.index]
+    print(index.iloc[df3.index])
+    df = df.iloc[:,df3.index]
+    df = df.iloc[df3.index,:]
+    df = df.set_index(index)
+    df.columns = index
+    print(df3.index)
+
+    plt.subplot(3,1,3)
+    sns.heatmap(df, fmt='g',cmap="coolwarm")
+    df['result'] = df3
+    #sns.heatmap(df)
+
+    plt.show()
+    df.to_csv('tanimoto_cluster.csv')
+
+if __name__ == '__main__':
+    coor_csv2cluster()
